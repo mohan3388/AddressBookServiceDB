cc072404dea2d3ddcb61ffeeb9a980c192b0d83c
mohan <sahumohan338@gmail.com>
2022-08-30 20:55:14 -0700

    [mohan]add: insert data in addressbook table

--- Database.sql
+++ Database.sql
@@ -18,3 +18,10 @@ PhoneNumber Varchar(10),
 Email varchar(150)
 );
 
+----UC3----
+Insert into Addressbook Values ('Rajesh','yadav','Bemetara','Durg','Chhattisgarh',490022,'7898625487','Mohan321@gmail.com'),
+('Pukesh','yadav','Berla','Durg','Chhattisgarh',542154,'8952241265','rajd99@gmail.com'),
+('murali','Upadhyay','gondiya','mumbai','Maharastra',675438,'8757889475','mural001@gmail.com'),
+('komal','patil','saibaba nagar','surat','gujarat',34567,'7854216785','komal222@gmail.com'),
+('riya','khairnar','shiv nagar','jaipur','Rajasthan',432007,'7285108928','riya8993@gmail.com');
+
