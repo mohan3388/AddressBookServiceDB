2668730b086a3151cd48d3fe91c5b7993c280bf7
mohan <sahumohan338@gmail.com>
2022-08-30 20:51:51 -0700

    [mohan]add: Create table Addressbook

--- Database.sql
+++ Database.sql
@@ -2,3 +2,19 @@
 create database AddressBookServiceDB
 
 use AddressBookServiceDB
+
+
+----UC2----
+create table Addressbook
+(
+Id int Primary Key Identity (1,1),
+FirstName VarChar (200),
+LastName varchar(150),
+Address varchar(250),
+City varchar(25),
+State varchar(60),
+ZipCode int,
+PhoneNumber Varchar(10),
+Email varchar(150)
+);
+
